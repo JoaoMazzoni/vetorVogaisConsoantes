04c9b01012088b1c62165ab5d21ff3ad6b17b94c
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-22 15:44:07 -0300

    Add project files.

---
+++ vetorVogaisConsoantes/Program.cs
@@ -0,0 +1,29 @@
+﻿char[] letra = new char[10];
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite uma letra: ");
+    letra[i] = char.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine("\nVogais: ");
+for (int i = 0; i < 10; i++)
+{
+
+    if (letra[i] == 'A' || letra[i] == 'E' || letra[i] == 'I' || letra[i] == 'O' || letra[i] == 'U')
+    {
+        Console.WriteLine(letra[i] + "");
+    }
+
+}
+
+Console.WriteLine("\nConsoantes: ");
+for (int i = 0; i < 10; i++)
+{
+    if (letra[i] != 'A' && letra[i] != 'E' && letra[i] != 'I' && letra[i] != 'O' && letra[i] != 'U')
+    {
+        Console.WriteLine(letra[i] + "");
+    }
+}
